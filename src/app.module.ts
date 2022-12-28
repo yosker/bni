8b1ad808493be18394f4a85232111096f35cd0c5
yosk zam <cristian.zaldivar@musicbattles.mx>
2022-12-28 10:47:18 -0600

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/bni'),
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
