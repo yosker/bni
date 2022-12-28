c64898aa639a26b6343217f4050e65d34a4f31fe
yosk zam <cristian.zaldivar@musicbattles.mx>
2022-12-28 10:44:05 -0600

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('API PRASYDE')
+    .setDescription('The Prasyde API description')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersSchema } from './schemas/users.schema';
+import { Users, UsersSchema } from './schemas/users.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
-        name: 'Users',
+        name: Users.name,
         schema: UsersSchema,
       },
     ]),
