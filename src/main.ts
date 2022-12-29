3a8e63d9d09d859a3ecdfdd57ba710d927fd5f27
yosk zam <cristian.zaldivar@musicbattles.mx>
2022-12-28 18:40:58 -0600

    main chapters

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Users')
     .addTag('Auth')
+    .addTag('Chapters')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('documentation', app, document);
