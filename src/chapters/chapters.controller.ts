9f10c7daf565ec3352304d8a5f8a2d8ab7aad647
yosk zam <cristian.zaldivar@musicbattles.mx>
2022-12-30 12:03:50 -0600

    se agrega promesa en controlador

--- src/chapters/chapters.controller.ts
+++ src/chapters/chapters.controller.ts
@@ -5,6 +5,7 @@ import { ApiTags } from '@nestjs/swagger';
 import { TokenJwt } from '../auth/decorators/token-jwt.decorator';
 import { JwtGuard } from '../auth/guards/jwt/jwt.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { ServicesResponse } from 'src/responses/response';
 
 @ApiTags('Chapters')
 @Controller('chapters')
@@ -14,7 +15,9 @@ export class ChaptersController {
   @Post('/create')
   @TokenJwt('test')
   // @UseGuards(AuthGuard(), JwtGuard)
-  async createPost(@Body() chapterDTO: CreateChapterDTO) {
+  async createPost(
+    @Body() chapterDTO: CreateChapterDTO,
+  ): Promise<ServicesResponse> {
     return await this.chapterService.createChapter(chapterDTO);
   }
 }
