30e32440cd95549de354ef3aa16528c0b0b972b1
Cristian Zaldivar <cristianzaldivar54@gmail.com>
2023-02-08 12:37:31 -0600

    se sube conexion a la base de datos atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 
 @Module({
@@ -10,7 +10,18 @@ import configuration from './config/configuration';
     ConfigModule.forRoot({
       load: [configuration],
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/bni'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => {
+        const host = config.get('database.host');
+        const credentials = config.get('database.credentials');
+        const name = config.get('database.name');
+        const uri = `mongodb+srv://${credentials}@${host}/${name}?retryWrites=true&w=majority`;
+        console.log(uri);
+        return { uri };
+      },
+      inject: [ConfigService],
+    }),
     UsersModule,
     AuthModule,
   ],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,10 @@
 export default () => ({
-  secret: process.env.JWT_SECRET,
-  port: parseInt(process.env.PORT, 10) || 3000,
+  secret: process.env.JWT_SECRET || 'y0sk3r1v01c313**',
+  port: process.env.PORT || 3002,
   database: {
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 27017,
+    name: process.env.DB_NAME || 'bni',
+    credentials: `${process.env.DB_USER}:${process.env.DB_PASSWORD}`
   },
 });
